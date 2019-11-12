71a49fb7b812cd726357d9f1a79961cc3eee90b8
nubranger <56818312+nubranger@users.noreply.github.com>
2019-11-12 10:17:49 +0200

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// header
+
+// hero
+
+// clients
+
+// about me
+
+// numbers
+
+// skills
+
+// latest work
+
+// job history
+
+// services
+
+// testimonials
+
+// contact me
+
+// footer
+
